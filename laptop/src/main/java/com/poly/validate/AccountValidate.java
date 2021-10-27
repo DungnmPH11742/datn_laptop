6dd7a422bf6f5fd0096cf84363f5d59013d0f713
DatTranVanQLDA <dattvph10333@fpt.edu.vn>
2021-10-27 22:02:05 +0700

    register
    update register

--- laptop/src/main/java/com/poly/validate/AccountValidate.java
+++ laptop/src/main/java/com/poly/validate/AccountValidate.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
-
 import java.util.regex.Pattern;
 
 @Component
