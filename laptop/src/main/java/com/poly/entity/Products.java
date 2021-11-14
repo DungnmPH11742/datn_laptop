50823e73e6b60d0a9799fe6c1e201cf59700296c
DungnmPH11742 <77944237+DungnmPH11742@users.noreply.github.com>
2021-11-14 22:32:53 +0700

    Update main
    Signed-off-by: DungnmPH11742 <77944237+DungnmPH11742@users.noreply.github.com>

--- laptop/src/main/java/com/poly/entity/Products.java
+++ laptop/src/main/java/com/poly/entity/Products.java
@@ -1,98 +1,13 @@
 package com.poly.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.sql.Date;
-import java.util.List;
 
 @Data
 @Entity
 @Table(name = "products")
 public class Products implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
-    @Id
-    @Column(name = "id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
-
-    @Column(name = "name", nullable = false)
-    private String name;
-
-    @Column(name = "date_on", nullable = false)
-    private Date dateOn;
-
-    @Column(name = "type_of_item", nullable = false)
-    private Integer typeOfItem;
-
-    @Column(name = "input_price")
-    private Float inputPrice;
-
-    @Column(name = "output_price")
-    private Float outputPrice;
-
-    @Column(name = "quantity")
-    private Integer quantity;
-
-    @Column(name = "mass")
-    private Float mass;
-
-    @Column(name = "unit", nullable = false)
-    private String unit;
-
-    @Column(name = "release_date")
-    private String releaseDate;
-
-    @Column(name = "date_of_manufacture")
-    private String dateOfManufacture;
-
-    @Column(name = "active")
-    private Boolean active;
-
-    //bi-directional many-to-one association to Description
-    @OneToMany(mappedBy="product")
-    private List<Description> descriptions;
-
-    //bi-directional many-to-one association to ImageDetail
-    @OneToMany(mappedBy="product")
-    private List<ImageDetail> imageDetails;
-
-    //bi-directional many-to-one association to OrderDetail
-    @OneToMany(mappedBy="product")
-    private List<OrderDetails> orderDetails;
-
-    //bi-directional many-to-one association to ProductRating
-//    @OneToMany(mappedBy="product")
-//    private List<ProductRating> productRatings;
-
-    //bi-directional many-to-one association to Category
-    @JsonIgnore
-    @ManyToOne
-    @JoinColumn(name="id_category")
-    private Category category;
-
-    //bi-directional many-to-one association to SaleProduct
-    @JsonIgnore
-    @ManyToOne
-    @JoinColumn(name="on_sale")
-    private SaleProduct saleProduct;
-
-    //bi-directional one-to-one association to ProductsDetail
-    @JsonIgnore
-    @OneToOne(mappedBy="product")
-    private ProductsDetail productsDetail;
-
-    //bi-directional many-to-one association to ShipmentDetail
-    @JsonIgnore
-    @OneToMany(mappedBy="product")
-    private List<ShipmentDetail> shipmentDetails;
-
-    //bi-directional many-to-one association to Voucher
-    @JsonIgnore
-    @OneToMany(mappedBy="product")
-    private List<Vouchers> vouchers;
 }
--- laptop/src/main/java/com/poly/repo/ProductsRepository.java
+++ laptop/src/main/java/com/poly/repo/ProductsRepository.java
@@ -3,24 +3,9 @@ package com.poly.repo;
 import com.poly.entity.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 @EnableJpaRepositories
 public interface ProductsRepository extends JpaRepository<Products, String>, JpaSpecificationExecutor<Products> {
 
-    @Query(value="SELECT * FROM ( SELECT * , ROW_NUMBER() OVER (ORDER BY id) AS RowNum FROM products  where type_of_item =?1) AS MyDerivedTable WHERE MyDerivedTable.RowNum BETWEEN 1 AND 12",
-            nativeQuery = true)
-    List<Products> getListByCate(Integer idCate);
-
-    @Query("select p from Products p where p.saleProduct.saleCode=:code")
-    List<Products> getListByCodeSale(@Param("code") String code);
-    List<Products> findAllByCategory_IdOrCategory_Id(Integer idCate, Integer idCate2);
-    List<Products> findAllByCategory_ParentId(Integer idCate);
-    List<Products> findAllByTypeOfItemAndCategory_ParentId(Integer type,Integer pantId );
-    List<Products> findAllByTypeOfItemAndCategory_Id(Integer type,Integer idCate );
-    List<Products> findAllByTypeOfItem(Integer id);
 }
