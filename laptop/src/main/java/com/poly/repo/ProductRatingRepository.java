288aefc17ba98a40c35d010fa4009c6b85f8859d
DungnmPH11742 <77944237+DungnmPH11742@users.noreply.github.com>
2021-11-15 21:47:04 +0700

    15/11
    Signed-off-by: DungnmPH11742 <77944237+DungnmPH11742@users.noreply.github.com>

--- laptop/src/main/java/com/poly/repo/ProductRatingRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.poly.repo;
-//
-//import com.poly.entity.ProductRating;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-//
-//public interface ProductRatingRepository extends JpaRepository<ProductRating, Void>, JpaSpecificationExecutor<ProductRating> {
-//
-//}
--- laptop/src/main/java/com/poly/service/impl/ProductServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.poly.service.impl;
-
-import com.poly.service.ProductService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ProductServiceImpl implements ProductService {
-
-}
